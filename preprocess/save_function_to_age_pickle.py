0f1d232d4454e2c5ddfb47185cf77ab82de086c8
angwhen <angelmwen@gmail.com>
2019-05-27 10:41:35 -0700

    graphs of distinct ages in specific dataset histogram

--- preprocess/save_function_to_age_pickle.py
+++ preprocess/save_function_to_age_pickle.py
@@ -11,23 +11,22 @@ def get_function_to_age_dict(gene_to_age_dict,function_to_genes_dict):
         genes = function_to_genes_dict[func.strip()]
         ages = list(map(lambda x: gene_to_age_dict[x.strip()] , genes)) 
         function_to_age_dict[func] = ages
-    print function_to_age_dict
+    #print function_to_age_dict
     return function_to_age_dict
 
 function_to_genes_dict = pickle.load(open("../data/function_to_genes_all_in_simplified.p","rb"))
 
 # for data from multiple datasets
-dataset_to_gene_to_age_dict = pickle.load(open("../data/dataset_to_gene_to_age_dict.p","rb"))  
+"""dataset_to_gene_to_age_dict = pickle.load(open("../data/dataset_to_gene_to_age_dict.p","rb"))  
 for dataset in dataset_to_gene_to_age_dict:
     gene_to_age_dict = dataset_to_gene_to_age_dict[dataset]
     function_to_age_dict = get_function_to_age_dict(gene_to_age_dict, function_to_genes_dict)
     pickle.dump(function_to_age_dict,open("../data/function_to_age_%s_dict.p"%dataset,"wb"))
+"""
 
 # for simplified data
 
-"""
 gene_to_age_simplified_dict = pickle.load(open("../data/gene_to_age_simplified_dict.p","rb"))
 function_to_age_dict = get_function_to_age_dict(gene_to_age_simplified_dict, function_to_genes_dict)
-print function_to_age_dict 
+#print function_to_age_dict 
 pickle.dump(function_to_age_dict,open("../data/function_to_age_simplified_dict.p","wb")
-"""
